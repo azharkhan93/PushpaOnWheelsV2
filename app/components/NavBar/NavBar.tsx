f3f2e915b900d3efcd93d4008ae87480b788c648
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-17 02:29:28 +0530

    pew pew

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,32 +1,72 @@
+"use client"
+import { useState } from 'react';
 import Image from 'next/image';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 export const Navbar = () => {
-    return (
-      <nav className="bg-black flex items-center justify-between px-8">
-        <div className="flex items-center" style={{ border: '1px solid green' }}>
-        <Image src="/images/push1.png" alt="Logo" width={100} height={50}
-         />
-          {/* <img src="../../assets/push1.png" alt="Logo" className="h-[90px]" /> */}
-        </div>
-  
-        <div className="flex-grow text-center hidden lg:flex lg:justify-center lg:gap-4">
-          <a href="/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-            Home
-          </a>
-          <a href="/about" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-            About
-          </a>
-          <a href="/services" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-            Services
-          </a>
-        </div>
-  
-        <button
-            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-11 rounded-l-lg rounded-r-lg rounded-b-2xl"
-            style={{ border: '1px solid white' }}
-          >
-            Explore it
-          </button>
-      </nav>
-    );
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
-  
+
+  return (
+    <nav className="bg-black flex items-center justify-between px-4">
+      <div className="flex items-center" >
+        <Image src="/images/push1.png" alt="Logo" width={100} height={50} />
+      </div>
+
+      <div className={`flex-grow text-center hidden lg:flex lg:justify-center lg:gap-4`}>
+        <a href="/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+          Home
+        </a>
+        <a href="/about" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+          About
+        </a>
+        <a href="/services" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+          Services
+        </a>
+      </div>
+
+      <div className="lg:hidden">
+        <button onClick={toggleMenu} className="text-gray-300 hover:text-white focus:outline-none">
+          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+      </div>
+
+      <div
+        className={`z-50 fixed top-0 left-0 h-[100vh] w-[100vw] bg-black text-white transform ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        } transition-transform duration-300 ease-in-out flex flex-col justify-center items-center space-y-6`}
+        
+      >
+        <button onClick={toggleMenu} className="text-gray-300 hover:text-white focus:outline-none absolute top-4 right-4">
+          <FaTimes size={28} />
+        </button>
+        <a href="/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-xl font-medium" onClick={toggleMenu}>
+          Home
+        </a>
+        <a href="/about" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-xl font-medium" onClick={toggleMenu}>
+          About
+        </a>
+        <a href="/services" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-xl font-medium" onClick={toggleMenu}>
+          Services
+        </a>
+        <button
+        className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-11 rounded-l-lg rounded-r-lg rounded-b-2xl"
+        style={{ border: '1px solid white' }}
+      >
+        Explore it
+      </button>
+      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-11 rounded-l-lg rounded-r-lg rounded-b-2xl lg:block hidden"
+        style={{ border: '1px solid white' }}
+      >
+        Explore it
+      </button>
+    </nav>
+  );
+};
+
+
