2eae9addc794f99d131e660313adbea3821ed3b3
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-17 03:09:54 +0530

    pew pew

--- app/components/ReviewsSection/ReviewsSection.tsx
+++ app/components/ReviewsSection/ReviewsSection.tsx
@@ -7,9 +7,9 @@ import { FaStar } from 'react-icons/fa';
 
 export const ReviewsSection = () => {
   return (
-    <div className='bg-black h-[100vh] flex items-center justify-center gap-11'>
+    <div className='bg-black h-auto lg:h-[100vh] flex items-center justify-center gap-11 lg:flex-row flex-col'>
     
-    <div className="w-[380px]  bg-white shadow-red-950 rounded-b-3xl rounded-t-3xl rounded-l-lg overflow-hidden   ">
+    <div className=" w-[334px] lg:w-[380px]  bg-white shadow-red-950 rounded-b-3xl rounded-t-3xl rounded-l-lg overflow-hidden   ">
       <div className="flex flex-col items-start p-2">
         <div className="  w-20 h-20 rounded-full overflow-hidden border-2 border-white">
           <img
@@ -31,7 +31,7 @@ export const ReviewsSection = () => {
         </div>
       </div>
     </div>
-    <div className="w-[380px]  bg-white shadow-red-950 rounded-b-3xl rounded-t-3xl rounded-l-lg overflow-hidden   ">
+    <div className="w-[334px] lg:w-[380px] bg-white shadow-red-950 rounded-b-3xl rounded-t-3xl rounded-l-lg overflow-hidden   ">
       <div className="flex flex-col items-start p-2">
         <div className="  w-20 h-20 rounded-full overflow-hidden border-2 border-white">
           <img
@@ -53,7 +53,7 @@ export const ReviewsSection = () => {
         </div>
       </div>
     </div>
-    <div className="w-[380px]  bg-white shadow-red-950 rounded-b-3xl rounded-t-3xl rounded-l-lg overflow-hidden   ">
+    <div className=" w-[334px] lg:w-[380px] bg-white shadow-red-950 rounded-b-3xl rounded-t-3xl rounded-l-lg overflow-hidden   ">
       <div className="flex flex-col items-start p-2">
         <div className="  w-20 h-20 rounded-full overflow-hidden border-2 border-white">
           <img
