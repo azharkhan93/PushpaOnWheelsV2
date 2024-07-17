2ca2824041d4841023a244b43a1afb11f88486e6
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-17 23:12:05 +0530

    pew pew

---
+++ app/components/MenuCard/MenuCard.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import { FaStar, FaShoppingCart } from 'react-icons/fa';
+type FoodCardProps = {
+    image: string;
+    title: string;
+    price: number;
+    rating: number;
+  }
+
+export const MenuCard: React.FC<FoodCardProps> = ({ image, title, price, rating }) => {
+  return (
+    <div className="bg-gray-800 text-white  shadow-lg p-4 gap-6 w-64 rounded-l-3xl">
+      <div className="flex justify-between items-center mb-4 gap-2">
+      <Image src={image} alt={title} width={64} height={64} className="rounded-full" />
+        <div className="text-lg font-bold">${price}</div>
+      </div>
+      <h3 className="text-xl font-semibold">{title}</h3>
+      <p className="text-sm text-gray-400">Per Plate</p>
+      <div className="flex items-center justify-between mt-2">
+        <div className="flex items-center justify-center gap-1">
+        <FaStar className="text-yellow-500 mr-1" />
+        <span>{rating}</span>
+        </div>
+        <div className="flex ">
+        <FaShoppingCart size={26} className="text-white cursor-pointer" />
+      </div>
+      </div>
+      
+    </div>
+  );
+};
+
+
--- app/components/MenuSection/MenuSection.tsx
+++ app/components/MenuSection/MenuSection.tsx
@@ -1,25 +1,44 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { MenuCard } from "../MenuCard";
 
-
-const menuItems = [
-    { name: "Paneer Momos", image: "/images/momos.jpg" },
-    { name: "Vegetable Momos", image: "/images/momo2.jpg" },
-    { name: "Peri Peri Momos", image: "/images/peri.webp" },
-    { name: "Malai Paneer Momos", image: "/images/momos.jpg" },
-    { name: "Vegetable Noodles", image: "/images/noodles.jpg" },
-    { name: "Haka Noodles", image: "/images/haka.jpg" },
-    { name: "Garlic Noodles", image: "/images/noodles.jpg" },
-    { name: "Cheese Chilli", image: "/images/cheese.jpg" },
-    { name: "Manchurian", image: "/images/mach.jpg" },
-    { name: "Mushroom Chilli", image: "/images/cheese.jpg" },
-    { name: "Fried Rice", image: "/images/rd.jpg" },
-    { name: "Fried Momos", image: "/images/fried.jpg" },
-  ];
+const foodItems = [
+  { image: '/images/cheese.jpg', title: 'Paneer Momos', price: 18, rating: 5.0 },
+  { image: '/images/momo2.jpg', title: 'Vegetable Momos', price: 12, rating: 3.8 },
+  { image: '/images/peri.webp', title: 'Peri Peri Momo', price: 24, rating: 4.9 },
+  { image: '/images/noodles.jpg', title: 'Malai Paneer Momos', price: 24, rating: 4.9 },
+  { image: '/images/fried.jpg', title: 'Fried Momos', price: 24, rating: 4.9 },
+  { image: '/images/noodles.jpg', title: 'Vegetable Noodles', price: 36, rating: 5.0 },
+  { image: '/images/haka.jpg', title: 'Haka Noodles', price: 16, rating: 4.8 },
+  { image: '/images/mach.jpg', title: 'Manchurian', price: 16, rating: 4.8 },
+  { image: '/images/cheese.jpg', title: 'Mushroom Chilli', price: 16, rating: 4.8 },
+  { image: '/images/fried.jpg', title: 'Fried Rice', price: 16, rating: 4.8 },
+  { image: '/images/cheese.jpg', title: 'Mushroom Chilli', price: 16, rating: 4.8 },
+  { image: '/images/noodles.jpg', title: 'Garlic Noodles ', price: 16, rating: 4.8 },
+  
+];
 export const MenuSection = () => {
   return (
     <>
-      <div
+    <div className="flex items-center justify-center flex-col py-20 bg-black gap-6 ">
+    <h2 className=" text-4xl lg:text-5xl font-semibold text-white text-start">Our <span className="text-yellow-900">Menu</span></h2>
+        <div className="w-[200px] h-1 bg-gradient-to-r from-green-400 to-blue-500 rounded-md "></div>
+        <p className="text-lg text-white mb-6  lg:w-[50%] w-[80%]">
+          Fill out the form below to book a table at our restaurant. We offer a variety of vegetarian foods and a top-notch dining experience.
+        </p>
+        <div className="flex items-center justify-center flex-wrap flex-row  bg-black gap-6">
+      {foodItems.map((item, index) => (
+        <MenuCard
+          key={index}
+          image={item.image}
+          title={item.title}
+          price={item.price}
+          rating={item.rating}
+        />
+      ))}
+      </div>
+    </div>
+      {/* <div
       id="Menu"
        className="flex items-center justify-center flex-col py-20 bg-black gap-6">
         <h2 className="text-5xl font-semibold text-white text-start">Our <span className="text-yellow-900">Menu</span></h2>
@@ -46,7 +65,7 @@ export const MenuSection = () => {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
