c7e04b912af3faedc269132f3b01d2dfe49fff1e
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-17 14:50:20 +0530

    pew pew

--- app/components/MenuSection/MenuSection.tsx
+++ app/components/MenuSection/MenuSection.tsx
@@ -1,113 +1,55 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 
+
+const menuItems = [
+    { name: "Paneer Momos", image: "/images/ks4.png" },
+    { name: "Vegetable Momos", image: "/images/ks4.png" },
+    { name: "Peri Peri Momos", image: "/images/ks4.png" },
+    { name: "Malai Paneer Momos", image: "/images/ks4.png" },
+    { name: "Vegetable Noodles", image: "/images/ks4.png" },
+    { name: "Haka Noodles", image: "/images/ks4.png" },
+    { name: "Garlic Noodles", image: "/images/ks4.png" },
+    { name: "Cheese Chilli", image: "/images/ks4.png" },
+    { name: "Manchurian", image: "/images/ks4.png" },
+    { name: "Mushroom Chilli", image: "/images/ks4.png" },
+    { name: "Fried Rice", image: "/images/ks4.png" },
+    { name: "Fried Momos", image: "/images/ks4.png" },
+  ];
 export const MenuSection = () => {
   return (
     <>
       <div className="flex items-center justify-center flex-col py-20 bg-black gap-6">
-      <h2 className="text-5xl font-semibold text-white text-start">Our Menu</h2>
-      <div className="w-[200px] h-1  bg-gradient-to-r from-green-400 to-blue-500 rounded-md ml-32"></div>
-          <p className="text-lg text-white mb-6 w-full lg:w-[50%]">Fill out the form below to book a table at our restaurant. We offer a variety of vegetarian foods and a top-notch dining experience.</p>
+        <h2 className="text-5xl font-semibold text-white text-start">Our Menu</h2>
+        <div className="w-[200px] h-1 bg-gradient-to-r from-green-400 to-blue-500 rounded-md ml-32"></div>
+        <p className="text-lg text-white mb-6 w-full lg:w-[50%]">
+          Fill out the form below to book a table at our restaurant. We offer a variety of vegetarian foods and a top-notch dining experience.
+        </p>
 
-        <div className="flex items-center justify-center  py-6 gap-5 flex-wrap mt-8">
-          <div className=" bg-red-950 shadow-lg overflow-hidden rounded-full shadow-emerald-950 flex justify-between gap-8 px-2 border-2 border-white py-1 ">
-            <div className="flex items-center ">
-              <img
-                src="/images/ks4.png"
-                alt="Menu Item Image"
-                className="h-[100px] w-30 object-cover rounded-full border-2 border-gray-200"
-              />
+        <div className="flex items-center justify-center py-6 gap-5 flex-wrap lg:mt-8 mt-2">
+          {menuItems.map((item, index) => (
+            <div key={index} className="bg-red-950 shadow-lg overflow-hidden rounded-b-3xl rounded-t-3xl rounded-r-3xl  shadow-emerald-950 flex flex-row items-center gap-4 px-4 py-2 border-2 border-green  w-[300px] ">
+              <div className="flex-shrink-0">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="lg:h-[100px] h-[70px] lg:w-[70px]  w-[70px] object-cover rounded-full border-2 border-gray-200"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h2 className="text-xl font-semibold text-white text-center truncate w-full">
+                  {item.name}
+                </h2>
+              </div>
             </div>
-            <div className="text-center flex items-center justify-center">
-              <h2 className="text-xl font-semibold text-white">
-                Menu Item Name
-              </h2>
-            </div>
-          </div>
-          <div className="shadow-lg overflow-hidden rounded-full shadow-emerald-950 flex justify-between gap-8 px-2 border-2 border-black py-1 ">
-            <div className="flex items-center ">
-              <img
-                src="/images/ks4.png"
-                alt="Menu Item Image"
-                className="h-[100px] w-30 object-cover rounded-full border-2 border-gray-200"
-              />
-            </div>
-            <div className="text-center flex items-center justify-center">
-              <h2 className="text-xl font-semibold text-gray-800">
-                Menu Item Name
-              </h2>
-            </div>
-          </div>
-          <div className="shadow-lg overflow-hidden rounded-full shadow-emerald-950 flex justify-between gap-8 px-2 border-2 border-black py-1 ">
-            <div className="flex items-center ">
-              <img
-                src="/images/ks4.png"
-                alt="Menu Item Image"
-                className="h-[100px] w-30 object-cover rounded-full border-2 border-gray-200"
-              />
-            </div>
-            <div className="text-center flex items-center justify-center">
-              <h2 className="text-xl font-semibold text-gray-800">
-                Menu Item Name
-              </h2>
-            </div>
-          </div>
-          <div className="shadow-lg overflow-hidden rounded-full shadow-emerald-950 flex justify-between gap-8 px-2 border-2 border-black py-1 ">
-            <div className="flex items-center ">
-              <img
-                src="/images/ks4.png"
-                alt="Menu Item Image"
-                className="h-[100px] w-30 object-cover rounded-full border-2 border-gray-200"
-              />
-            </div>
-            <div className="text-center flex items-center justify-center">
-              <h2 className="text-xl font-semibold text-gray-800">
-                Menu Item Name
-              </h2>
-            </div>
-          </div>
-          <div className="shadow-lg overflow-hidden rounded-full shadow-emerald-950 flex justify-between gap-8 px-2 border-2 border-black py-1 ">
-            <div className="flex items-center ">
-              <img
-                src="/images/ks4.png"
-                alt="Menu Item Image"
-                className="h-[100px] w-30 object-cover rounded-full border-2 border-gray-200"
-              />
-            </div>
-            <div className="text-center flex items-center justify-center">
-              <h2 className="text-xl font-semibold text-gray-800">
-                Menu Item Name
-              </h2>
-            </div>
-          </div>
-          <div className="shadow-lg overflow-hidden rounded-full shadow-emerald-950 flex justify-between gap-8 px-2 border-2 border-black py-1 ">
-            <div className="flex items-center ">
-              <img
-                src="/images/ks4.png"
-                alt="Menu Item Image"
-                className="h-[100px] w-30 object-cover rounded-full border-2 border-gray-200"
-              />
-            </div>
-            <div className="text-center flex items-center justify-center">
-              <h2 className="text-xl font-semibold text-gray-800">
-                Menu Item Name
-              </h2>
-            </div>
-          </div>
-
-          
-
-          
-
-          
-
-    
-
-          
+          ))}
         </div>
       </div>
     </>
   );
 };
+
+
+
 
 
