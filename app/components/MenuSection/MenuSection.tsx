eee88fbb9c69e94f255e9a7302e5788d29485ce9
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-19 23:28:55 +0530

    menus cartd updated

--- app/components/MenuSection/MenuSection.tsx
+++ app/components/MenuSection/MenuSection.tsx
@@ -1,22 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { MenuCard } from "../MenuCard";
+import { foodItems } from "@/constants";
 
-const foodItems = [
-  { image: '/images/cheese.jpg', title: 'Paneer Momos', price: 18, rating: 5.0 },
-  { image: '/images/momo2.jpg', title: 'Vegetable Momos', price: 12, rating: 3.8 },
-  { image: '/images/peri.webp', title: 'Peri Peri Momo', price: 24, rating: 4.9 },
-  { image: '/images/noodles.jpg', title: 'Malai Paneer Momos', price: 24, rating: 4.9 },
-  { image: '/images/fried.jpg', title: 'Fried Momos', price: 24, rating: 4.9 },
-  { image: '/images/noodles.jpg', title: 'Vegetable Noodles', price: 36, rating: 5.0 },
-  { image: '/images/nod.jpg', title: 'Haka Noodles', price: 16, rating: 4.8 },
-  { image: '/images/mach.jpg', title: 'Manchurian', price: 16, rating: 4.8 },
-  { image: '/images/cheese.jpg', title: 'Mushroom Chilli', price: 16, rating: 4.8 },
-  { image: '/images/fried.jpg', title: 'Fried Rice', price: 16, rating: 4.8 },
-  { image: '/images/cheese.jpg', title: 'Mushroom Chilli', price: 16, rating: 4.8 },
-  { image: '/images/noodles.jpg', title: 'Garlic Noodles ', price: 16, rating: 4.8 },
-  
-];
+
 export const MenuSection = () => {
   return (
     <>
--- constants.ts
+++ constants.ts
@@ -22,3 +22,19 @@ export const navLinks = [
     reviewText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
   }
   ]
+
+  export const foodItems = [
+    { image: '/images/cheese.jpg', title: 'Paneer Momos', price: 18, rating: 5.0 },
+    { image: '/images/momo2.jpg', title: 'Vegetable Momos', price: 12, rating: 3.8 },
+    { image: '/images/peri.webp', title: 'Peri Peri Momo', price: 24, rating: 4.9 },
+    { image: '/images/noodles.jpg', title: 'Malai Paneer Momos', price: 24, rating: 4.9 },
+    { image: '/images/fried.jpg', title: 'Fried Momos', price: 24, rating: 4.9 },
+    { image: '/images/noodles.jpg', title: 'Vegetable Noodles', price: 36, rating: 5.0 },
+    { image: '/images/nod.jpg', title: 'Haka Noodles', price: 16, rating: 4.8 },
+    { image: '/images/mach.jpg', title: 'Manchurian', price: 16, rating: 4.8 },
+    { image: '/images/cheese.jpg', title: 'Mushroom Chilli', price: 16, rating: 4.8 },
+    { image: '/images/fried.jpg', title: 'Fried Rice', price: 16, rating: 4.8 },
+    { image: '/images/cheese.jpg', title: 'Mushroom Chilli', price: 16, rating: 4.8 },
+    { image: '/images/noodles.jpg', title: 'Garlic Noodles ', price: 16, rating: 4.8 },
+    
+  ];
