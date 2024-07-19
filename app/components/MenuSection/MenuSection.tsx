d4689c1ea2d2059d0f3f81ceb1d11e9bd35448d0
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-19 23:36:47 +0530

    ,menusection

--- app/components/MenuSection/MenuSection.tsx
+++ app/components/MenuSection/MenuSection.tsx
@@ -3,34 +3,36 @@ import React from "react";
 import { MenuCard } from "../MenuCard";
 import { foodItems } from "@/constants";
 
-
 export const MenuSection = () => {
   return (
     <>
-    <div className="flex items-center justify-center flex-col py-20 bg-black gap-6 ">
-    <h2 className=" text-4xl lg:text-5xl font-semibold text-white text-start">Our <span className="text-yellow-900">Menu</span></h2>
+      <div className="flex items-center justify-center flex-col py-20 bg-black gap-6 ">
+        <h2 className=" text-4xl lg:text-5xl font-semibold text-white text-start">
+          Our <span className="text-yellow-900">Menu</span>
+        </h2>
         <div className="w-[200px] h-1 bg-gradient-to-r from-green-400 to-blue-500 rounded-md "></div>
         <p className="text-lg text-white mb-6  lg:w-[50%] w-[80%]">
-          Fill out the form below to book a table at our restaurant. We offer a variety of vegetarian foods and a top-notch dining experience.
+          Discover our exclusive <span className="text-yellow-900">Menu</span>{" "}
+          and enjoy a top-notch dining experience. We offer a variety of
+          vegetarian foods, each dish meticulously crafted with fresh, locally
+          sourced ingredients. Whether you are looking for a hearty meal or a
+          light bite, our menu has something to satisfy every palate. Indulge in
+          our specials and experience flavors that will leave you craving for
+          more. Fill out the form below to book a table at our restaurant and
+          embark on a culinary journey like no other
         </p>
         <div className="flex items-center justify-center flex-wrap flex-row  bg-black gap-6">
-      {foodItems.map((item, index) => (
-        <MenuCard
-          key={index}
-          image={item.image}
-          title={item.title}
-          price={item.price}
-          rating={item.rating}
-        />
-      ))}
+          {foodItems.map((item, index) => (
+            <MenuCard
+              key={index}
+              image={item.image}
+              title={item.title}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-      
     </>
   );
 };
-
-
-
-
-
