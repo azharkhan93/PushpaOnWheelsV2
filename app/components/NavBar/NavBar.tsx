0eee985d09bbfe3e349905536e54dd7142bba7c1
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-20 23:23:59 +0530

    nvi buttons fixed

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -61,12 +61,12 @@ export const Navbar = () => {
         </button>
         </a>
       </div>
-      <div>
-       
+      
       <button className="bg-black hover:bg-gray-950 text-white py-2 px-11 rounded-r-3xl rounded-b-3xl border-2 border-white lg:block hidden">
         Book Table
-      </button> 
-      </div>
+      </button>
+      
+      
     </nav>
   );
 };
