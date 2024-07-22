9712b9ec517d971a9dfd12282b10d44431cb7dde
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-23 00:29:30 +0530

    rigs

--- app/components/ReviewsSection/ReviewsSection.tsx
+++ app/components/ReviewsSection/ReviewsSection.tsx
@@ -15,11 +15,11 @@ export const ReviewsSection = () => {
     className='bg-black h-auto lg:h-[100vh] flex items-center justify-center  gap-4 flex-col '>
       
       
-      <h2 className="text-4xl lg:text-5xl text-white text-start mt-8 reveal reveal-from-left">
+      <h2 className="text-4xl lg:text-5xl text-white text-start mt-8 ">
         Client <span className='text-magenta' >Reviews</span>
       </h2>
         <div className="w-[200px] h-1  bg-gradient-to-r from-green-400 to-blue-500 rounded-md "></div>
-          <p className="text-lg text-white mb-6  w-[80%] lg:w-[50%] font-quicksand capitalize reveal reveal-from-left"> See what our clients have to say about their experience at our restaurant.</p>
+          <p className="text-lg text-white mb-6  w-[80%] lg:w-[50%] font-quicksand capitalize"> See what our clients have to say about their experience at our restaurant.</p>
 
           <div className="flex items-center justify-center lg:flex-row gap-11 flex-col py-8 ">
         {reviews.map((review, index) => (
