ceadacbc2c175f4cedead9145c55645830604f08
azharkhan93 <azharkhan.work101@gmail.com>
2024-07-24 23:58:33 +0530

    craxy change

--- app/components/InputField/InputField.tsx
+++ app/components/InputField/InputField.tsx
@@ -1,15 +1,14 @@
-"use client"
-import { Field, ErrorMessage } from 'formik';
+"use client";
+import { Field, ErrorMessage } from "formik";
 type FormFieldProps = {
-    label: string;
-    name: string;
-    type: string;
-  }
+  label: string;
+  name: string;
+  type: string;
+};
 
-export const InputField:React.FC<FormFieldProps> = ({label, name , type}) => {
-    return (
-
-        <div className="mb-4">
+export const InputField: React.FC<FormFieldProps> = ({ label, name, type }) => {
+  return (
+    <div className="mb-4">
       <label htmlFor={name} className="block text-white">
         {label}
       </label>
@@ -19,9 +18,11 @@ export const InputField:React.FC<FormFieldProps> = ({label, name , type}) => {
         name={name}
         className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
-      <ErrorMessage name={name} component="div" className="text-red-500 text-sm mt-1" />
+      <ErrorMessage
+        name={name}
+        component="div"
+        className="text-red-500 text-sm mt-1"
+      />
     </div>
-
-
-    );
-}
+  );
+};
